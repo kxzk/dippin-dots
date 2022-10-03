1216f46e9346f18ab6c21197f40942df8c043c0f
kadekillary <kadekillary@pm.me>
2022-10-03 03:48:45 -0700

    Update: make msg area readable

--- nvim/.config/nvim/lua/user/theme.lua
+++ nvim/.config/nvim/lua/user/theme.lua
@@ -7,4 +7,4 @@ cmd [[highlight LineNr guifg=#3B4252]]          -- dim line numbers
 cmd [[highlight CursorLineNr guifg=#EBCB8B]]    -- brighter cursorline number
 cmd [[highlight EndOfBuffer guifg=#2E3440]]     -- dim tilde under line number
 cmd [[highlight VertSplit guifg=#EBCB8B gui=None guibg=None]]   -- remove bad split coloring
-cmd [[highlight MsgArea guifg=#3B4252]]         -- dim command line/message area
+cmd [[highlight MsgArea guifg=#EBCB8B]]         -- dim command line/message area
