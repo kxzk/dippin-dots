f5437185084296fd1f2fabd4c7db0bfb8e4dcdde
kadekillary <kadekillary@pm.me>
2022-10-10 12:56:16 -0700

    Add: checkports shortcut

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -28,6 +28,7 @@ alias tree="exa -lxT --no-permissions --no-time --no-user --no-filesize"
 alias treesize="exa -lxT --no-permissions --no-time --no-user"
 alias modelsize="exa -lxR --no-permissions --no-time --no-user --sort filesize --ignore-glob='*.yml|*.md' --reverse | awk '/^[1-9]/' | sort -hr"
 alias jli="jira issue list -a$(jira me)"
+alias checkports="lsof -i -n -P | grep TCP"
 
 # shortcuts
 alias snowsql="/Applications/SnowSQL.app/Contents/MacOS/snowsql"
