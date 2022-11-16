60fb1306a22154c93d29f370bd81b4f92d583b65
kadekillary <kadekillary@pm.me>
2022-11-16 12:50:29 -0800

    Update open to pbcopy

--- fish/.config/fish/functions/prlink.fish
+++ fish/.config/fish/functions/prlink.fish
@@ -1,5 +1,5 @@
 function prlink
     set -l alert (echo "@$argv")
     set -l prlinks (gh pr list --author '@me' --json 'title' --json 'url' | jq '.[]' | jq -r '"\(.url) -> \(.title)"')
-    begin echo $alert; echo \n$prlinks; end | open -e -f
+    begin echo $alert; echo \n$prlinks; end | pbcopy
 end
