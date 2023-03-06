f75dd04d5b2b06758dfd47bf049b68da9dcb02fa
kadekillary <kadekillary@pm.me>
2023-03-06 04:36:36 -0800

    Add quicker sqlfmt formatting

--- nvim/.config/nvim/lua/user/augroups.lua
+++ nvim/.config/nvim/lua/user/augroups.lua
@@ -36,7 +36,7 @@ vim.cmd [[
     augroup _sql
         autocmd!
         autocmd Filetype sql nmap <leader>r :20 split term://snowsql -f %<CR>
-        autocmd Filetype sql nmap <leader>t :!sqlfmt %<CR>>
+        autocmd Filetype sql nmap <leader>t :!sqlfmt %<CR><CR>
     augroup end
 
     augroup _terminal
