682cb9a232e066599233c951c55182f1b532751d
kadekillary <kadekillary@pm.me>
2023-03-21 10:41:51 -0700

    Add isort and git pull shortcut

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -21,6 +21,7 @@ alias pipu="pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip
 
 # util
 alias g="git"
+alias gp="git pull"
 alias vim="nvim"
 alias vi="nvim"
 alias ib="issue_branch"
