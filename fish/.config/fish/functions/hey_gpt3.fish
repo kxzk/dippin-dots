be18f6f88521b2108f778c0312fd2023da3847f3
kadekillary <kadekillary@pm.me>
2023-04-04 22:49:52 -0700

    Update log gpt to gh gist

--- fish/.config/fish/functions/hey_gpt3.fish
+++ fish/.config/fish/functions/hey_gpt3.fish
@@ -1,14 +1,15 @@
-function hey_gpt
+function hey_gpt3
     set prompt \'(echo $argv | string join ' ')\'
     set gpt (curl https://api.openai.com/v1/chat/completions -s \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer $OPENAI_KEY" \
     -d '{
-        "model": "gpt-4",
+        "model": "gpt-3.5-turbo",
         "messages": [{"role": "user", "content": "'$prompt'"}],
-        "temperature": 0.7,
+        "temperature": 1.0,
         "stream": true
     }')
+    set output ''
     for text in $gpt
         if test $text = 'data: [DONE]'
             break
