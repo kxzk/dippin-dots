86334f3055aa31cf54c88a228969514395427e3d
kade <kadekillary@pm.me>
2023-06-10 17:29:39 -0700

    Update restow_dirs.fish

--- fish/.config/fish/functions/restow_dirs.fish
+++ fish/.config/fish/functions/restow_dirs.fish
@@ -1,5 +1,5 @@
 function restow_dirs
-    set -l packages alacritty fish git nvim tmux duckdb
+    set -l packages alacritty fish git nvim tmux duckdb hushlogin
     for p in $packages
         stow -Rv $p
     end
