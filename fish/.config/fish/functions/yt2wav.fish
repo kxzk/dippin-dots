1b0062ea03fd7215a0ada382d27a95ed520901f7
kadekillary <kadekillary@pm.me>
2024-02-01 23:15:25 -0800

    add y2wav func

---
+++ fish/.config/fish/functions/yt2wav.fish
@@ -0,0 +1,14 @@
+function yt2wav
+    # Check if URL is provided
+    if not set -q argv[1]
+        echo "Usage: youtube_to_wav <YouTube URL>"
+        exit 1
+    end
+
+    # YouTube Video URL
+    set URL $argv[1]
+
+    # Download the best audio quality using yt-dlp and convert to WAV using FFmpeg
+    yt-dlp -x --audio-format wav --audio-quality 0 -o "%(title)s.%(ext)s" "$URL"
+end
+
