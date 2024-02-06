a9acf14b3245c44b9be0c33d7a5f370fb48beed0
kadekillary <kadekillary@pm.me>
2024-02-05 23:51:52 -0800

    update oai key name

--- fish/.config/fish/functions/hey_gpt3.fish
+++ fish/.config/fish/functions/hey_gpt3.fish
@@ -2,7 +2,7 @@ function hey_gpt3
     set prompt \'(echo $argv | string join ' ')\'
     set gpt (curl https://api.openai.com/v1/chat/completions -s \
     -H "Content-Type: application/json" \
-    -H "Authorization: Bearer $OPENAI_KEY" \
+    -H "Authorization: Bearer $OPENAI_API_KEY" \
     -d '{
         "model": "gpt-3.5-turbo",
         "messages": [{"role": "user", "content": "'$prompt'"}],
