868a8739329ef70b83c546c7cb33deaba5a20284
kadekillary <kadekillary@pm.me>
2024-08-27 01:37:49 -0700

    patch berk mono, add back markdown for avante

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -140,19 +140,22 @@ return {
 	{
 		"yetone/avante.nvim",
 		event = "VeryLazy",
-		build = "make",
+		build = "make", -- This is Optional, only if you want to use tiktoken_core to calculate tokens count
 		opts = {
 			-- add any opts here
 		},
 		dependencies = {
-			"nvim-tree/nvim-web-devicons",
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
 			"stevearc/dressing.nvim",
 			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below is optional, make sure to setup it properly if you have lazy=true
 			{
-				"grapp-dev/nui-components.nvim",
-				dependencies = {
-					"MunifTanjim/nui.nvim",
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
 				},
+				ft = { "markdown", "Avante" },
 			},
 		},
 	},
