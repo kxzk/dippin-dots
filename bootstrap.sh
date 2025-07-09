c8169f6edb5a31d22b14893dde40d06b533b5e9d
kadekillary <kadekillary@pm.me>
2025-07-09 15:48:49 -0700

    remove alacritty setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,25 +5,23 @@ set -o nounset   # abort on unbound variable
 set -o pipefail  # don't hide errors within pipes
 
 bootstrap() {
-	# xcode-select --install
-	# sudo xcodebuild -license accept
+	xcode-select --install
+	sudo xcodebuild -license accept
 
 	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-	
-	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/edak/.zprofile
+
+	echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/kade.killary/.zprofile
 	source .zprofile
 
 	brew install stow
-	
-	brew install --cask alacritty --no-quarantine
-	
+
 	brew bundle
-	
+
 	echo /opt/homebrew/bin/fish | sudo tee -a /etc/shells
 	chsh -s /opt/homebrew/bin/fish
-	
+
 	fish_add_path /opt/homebrew/bin
-	
+
 	rm -rf .config/fish
 }
 
